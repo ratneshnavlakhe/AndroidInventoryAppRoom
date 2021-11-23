321d94437565319242b1d64ca7c9e20812d1fc16
Ratnesh Navlakhe <ratnesh061288@gmail.com>
2021-11-23 14:54:17 +0530

    Add new database entity, dao, database, viewmodel for inserting the data to the dataabase

---
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -0,0 +1,24 @@
+package com.example.inventory.data
+
+import androidx.room.*
+import androidx.room.OnConflictStrategy.IGNORE
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+
+    @Insert(onConflict = IGNORE)
+    suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("SELECT * FROM item WHERE id = :id")
+    fun getItemId(id: Int): Flow<Item>
+
+    @Query("SELECT * FROM item ORDER BY name ASC")
+    fun getItems(): Flow<List<Item>>
+}
